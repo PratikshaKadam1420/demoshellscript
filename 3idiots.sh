312865b13cf3caec3a4d6ada8149706d5a5be9ad
Ubuntu <ubuntu@ip-172-31-42-168.eu-west-1.compute.internal>
2025-02-15 06:14:28 +0000

    script added successfully

---
+++ 3idiots.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+hero="rancho"
+villan="virus"
+
+echo " 3 idiots ka hero hai $hero"
+
+echo "3idiots ka villian hai $viilian"
+
+#shell / enviorment variable 
+
+echo "current logged in user $USER"
+
+#userinput
+read -p "rancho ch purn nav kay hot?" fullname
+
+echo "Racho ch purn nav $fullname hot"
+
+#arguments 
+
+# ./3idiots.sh raju farhan rancho 
+
+echo " moview ch name $0"
+echo "1st idiot $1"
+echo "2nd idiot $2"
+echo "3rd idiot $3"
+echo " all idiots $@"
+
+
+
+
